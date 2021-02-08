96463376bcbf0f9bd1718717901778cd4c968b32
Kevin Litman-Navarro <kevin@Kevins-MacBook-Pro-2.local>
2021-02-08 14:54:15 -0800

    update github pages

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -3,29 +3,44 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <meta name="description" content="" />
+  <meta
+    name="description"
+    content="An auditory comparison of 770 artists' live and studio tracks"
+  />
   <meta name="news_keywords" content="" />
 
-  <meta property="og:title" content="" />
-  <meta property="og:site_name" content="" />
-  <meta property="og:url" content="" />
-  <meta property="og:description" content="description" />
+  <meta property="og:title" content="Live Music Jukebox" />
+  <meta property="og:site_name" content="The Pudding" />
+  <meta property="og:url" content="https://pudding.cool/2021/02/jukebox/" />
+  <meta
+    property="og:description"
+    content="An auditory comparison of 770 artists' live and studio tracks"
+  />
   <meta property="og:type" content="article" />
   <meta property="og:locale" content="en_US" />
 
-  <meta property="og:image" content="" />
+  <meta
+    property="og:image"
+    content="https://pudding.cool/2021/02/jukebox/assets/facebook@2x.jpg"
+  />
   <meta property="og:image:type" content="image/jpeg" />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="600" />
 
   <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="" />
-  <meta name="twitter:creator" content="" />
-  <meta name="twitter:title" content="" />
-  <meta name="twitter:description" content="" />
-  <meta name="twitter:image:src" content="" />
+  <meta name="twitter:site" content="https://pudding.cool" />
+  <meta name="twitter:creator" content="@puddingviz" />
+  <meta name="twitter:title" content="Live Music Jukebox" />
+  <meta
+    name="twitter:description"
+    content="An auditory comparison of 770 artists' live and studio tracks"
+  />
+  <meta
+    name="twitter:image:src"
+    content="https://pudding.cool/2021/02/jukebox/assets/twitter@2x.jpg"
+  />
 
   <meta name="robots" content="max-image-preview:large" />
 
-  <link rel="canonical" href="" />
+  <link rel="canonical" href="https://pudding.cool/2021/02/jukebox/" />
 </svelte:head>
